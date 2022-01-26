befd158130f445f2a106981a77d24a118ef99a36
Camilo Cuitiva <camiloandres174@gmail.com>
2022-01-26 15:35:49 -0500

    Add Project

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'stroke': '#1c2934',
+        'main': '#fffffe',
+        'highlight': '#6bad9b',
+        'secondary': '#2b2c34',
+        'tertiary': '#6246ea',
+      }
+    },
+  },
+  plugins: [],
+}
